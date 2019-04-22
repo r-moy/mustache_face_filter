3d5302d7b61b88fbc084696817d2754c5bd4daff
raymondmoy <rymoy2@illinois.edu>
2019-04-22 13:21:16 -0500

    Initial Commit

---
+++ src/controls.h
@@ -0,0 +1,13 @@
+//
+//  controls.hpp
+//  CS126FinalProject
+//
+//  Created by Raymond Moy on 4/21/19.
+//
+
+#ifndef controls_hpp
+#define controls_hpp
+
+#include <stdio.h>
+
+#endif /* controls_hpp */
---
+++ src/pacMan.h
@@ -0,0 +1,13 @@
+//
+//  pacMan.hpp
+//  CS126FinalProject
+//
+//  Created by Raymond Moy on 4/21/19.
+//
+
+#ifndef pacMan_hpp
+#define pacMan_hpp
+
+#include <stdio.h>
+
+#endif /* pacMan_hpp */
