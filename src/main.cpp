8b41571b2802013fb1cf3c809838e7751be64973
raymondmoy <rymoy2@illinois.edu>
2019-04-23 18:38:48 -0500

    added in example files to test with

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-#include "ofMain.h"
 #include "ofApp.h"
 
-//========================================================================
-int main( ){
-	ofSetupOpenGL(448,576,OF_WINDOW);			// <-------- setup the GL context
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
-
+int main() {
+    ofSetupOpenGL(640, 960, OF_WINDOW);
+    return ofRunApp(std::make_shared<ofApp>());
 }
